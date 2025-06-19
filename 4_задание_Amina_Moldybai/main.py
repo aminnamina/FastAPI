7378ee62dcdc68fd16f1db831b92e8dbdf7cb036
LINESKL <priveth06@gmail.com>
2025-06-19 15:13:17 +0500

    4 task

---
+++ 4_задание_Amina_Moldybai/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlmodel import Field, SQLModel, Session, select
+from typing import Optional, Annotated
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from data import engine, pwd_context, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM, oauth2_scheme, JWTError
+from datetime import timedelta
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    username: str = Field(index=True, unique=True, min_length=3, max_length=50)
+    password: str = Field(min_length=8)
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+def get_user(username: str, db: Session):
+    return db.exec(select(User).where(User.username == username)).first()
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+@app.post("/register/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user(user.username, db)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already registered"
+        )
+
+    user.password = hash_password(user.password)
+    new_user = User(username=user.username, password=user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.post("/login/", response_model=Token)
+def login(credentials: UserLogin, db: Annotated[Session, Depends(get_db)]):
+    user = get_user(credentials.username, db)
+    if not user or not verify_password(credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
